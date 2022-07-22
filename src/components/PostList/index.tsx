afb9ab19568c6c23113be4f33c5c98a564bfd920
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-07-22 13:25:55 +0530

    add table for posts lists

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,38 +1,88 @@
+import { useEffect, useState } from "react"
 import { usePosts } from "hooks"
-import type { Post } from 'types'
 
+// components
+import {
+  Button,
+  Container,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react"
 
-const PostListItem = ({ id, userId, title, body}: Post) => (
+// types
+import type { PostSummary } from "types"
+
+//----------------------------------------------------------------
+
+const PostListItem = ({ id, userId, title, completed }: PostSummary) => (
   <>
-    <p>title: {title}</p>
+    <Tr>
+      <Td>
+        <Text textAlign={"left"} noOfLines={1}>
+          {title}
+        </Text>
+      </Td>
+      <Td textAlign='center'>{userId}</Td>
+      <Td textAlign='center'>{completed ? "Yes" : "No"}</Td>
+    </Tr>
   </>
 )
 
 const PostList = () => {
-    const { loading, error, posts } = usePosts()
+  const { loading, error, posts: postData } = usePosts()
+  const [posts, setPosts] = useState<PostSummary[]>([])
 
-    if (loading) {
-      return <p>loading...</p>
-    } 
-    if (error != null) {
-      return <p>error: {error.message}</p>
-    }
+  useEffect(() => {
+    setPosts(postData)
+  }, [postData])
 
-    return(
-        <>
-          <p>Post list</p>
-          {
-            posts.map((post) => (
-              <PostListItem 
-                id={post.id}
-                userId={post.userId}
-                title={post.title}
-                body={post.body}
-              />
-            ))
-          }
-        </>
-    )
+  if (error) {
+    return <p>error: {error}</p>
+  }
+  if (loading) {
+    return <p>loading...</p>
+  }
+
+  const addPost = async (event: React.MouseEvent) => {
+    // setPosts([...posts, { id: 3, userId:100, title: 'Hello', body: 'World' }])
+  }
+
+  return (
+    <>
+      <Container maxW='4xl' border='solid 1px green'>
+        <Button onClick={addPost}>add post</Button>
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>Title</Th>
+                <Th>UserId</Th>
+                <Th>Completed</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {posts &&
+                posts.map((post) => (
+                  <PostListItem
+                    key={post.id}
+                    id={post.id}
+                    userId={post.userId}
+                    title={post.title}
+                    completed={post.completed}
+                  />
+                ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Container>
+    </>
+  )
 }
 
 export default PostList
--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,22 +1,30 @@
 import { useState, useEffect } from 'react'
 
-import type { Post } from 'types'
+import type { Post, PostSummary } from 'types'
+
+
+async function fetchJSON(url: string, opts?: RequestInit) {
+    const res = await fetch(url, opts)
+    const data = await res.json()
+    console.log("Data is like: ", data.slice(0, 5))
+    return data
+}
 
 /**
  * hook to fetch posts - also indicates if 'loading' or 'error'
  * to enable appropriate UI changes 
 */
 function usePosts() {
-    const [posts, setPosts] = useState<Post[]>([])
+    const [posts, setPosts] = useState<PostSummary[]>([])
     const [loading, setLoading] = useState<boolean>(false)
-    const [error, setError] = useState<Error | null>(null)
+    const [error, setError] = useState<string>('')
 
     useEffect(() => {
         setLoading(true)
-        fetch("https://jsonplaceholder.typicode.com/todos")
-            .then((res) => res.json())
+        fetchJSON("https://jsonplaceholder.typicode.com/todos")
+            .then((data) => data.slice(0, 10)) // util dev mode
             .then((data) => setPosts(data))
-            .catch(err => setError(err))
+            .catch((err: Error) => setError(err.message))
             .finally(() => setLoading(false))
     }, [])
 
