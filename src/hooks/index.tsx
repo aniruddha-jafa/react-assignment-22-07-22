cc424e18cd93d20a29886f35c70629326dea433c
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-07-22 15:35:25 +0530

    hook for fetching post

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,36 +1,62 @@
 import { useState, useEffect } from 'react'
 
-import type { Post, PostSummary } from 'types'
-
+import type { Post } from "types"
 
 async function fetchJSON(url: string, opts?: RequestInit) {
-    const res = await fetch(url, opts)
-    const data = await res.json()
-    console.log("Data is like: ", data.slice(0, 5))
-    return data
+  const res = await fetch(url, opts)
+  const data = await res.json()
+  return data
 }
 
 /**
- * hook to fetch posts - also indicates if 'loading' or 'error'
- * to enable appropriate UI changes 
-*/
+ * hook to fetch all posts - also indicates if 'loading' or 'error'
+ * to enable appropriate UI changes
+ */
 function usePosts() {
-    const [posts, setPosts] = useState<PostSummary[]>([])
-    const [loading, setLoading] = useState<boolean>(false)
-    const [error, setError] = useState<string>('')
+  const [posts, setPosts] = useState<Post[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>("")
 
-    useEffect(() => {
-        setLoading(true)
-        fetchJSON("https://jsonplaceholder.typicode.com/todos")
-            .then((data) => data.slice(0, 10)) // util dev mode
-            .then((data) => setPosts(data))
-            .catch((err: Error) => setError(err.message))
-            .finally(() => setLoading(false))
-    }, [])
+  useEffect(() => {
+    setLoading(true)
+    fetchJSON("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => data.slice(0, 10)) // util dev mode
+      .then((data) => setPosts(data))
+      .catch((err: Error) => setError(err.message))
+      .finally(() => setLoading(false))
+  }, [])
 
-    return {
-        posts, loading, error
-    }
+  return {
+    posts,
+    loading,
+    error,
+  }
 }
 
-export { usePosts}
+/**
+ * Fetch a single post
+ */
+function usePost(id: string) {
+  const [post, setPost] = useState<Post | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>("")
+
+  useEffect(() => {
+    setLoading(true)
+    fetchJSON(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((data) => {
+        console.debug("Post data: ", data)
+        setPost(data)
+      })
+      .catch((err: Error) => setError(err.message))
+      .finally(() => setLoading(false))
+  }, [id])
+
+  return {
+    post,
+    loading,
+    error,
+  }
+}
+
+export { usePosts, usePost }
